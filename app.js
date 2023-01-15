64ee03dd8b9f29ccb604bdabe3a08037c4f163dd
ratankaliani <ratankaliani@berkeley.edu>
2023-01-14 16:01:59 -0800

    able to relay vote correctly

--- app.js
+++ app.js
@@ -1,11 +1,142 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-import api from './api'
+const ethers = require("ethers")
+const privPollABI = require("./abi/privpoll1.json")
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+// const bodyParser = require('body-parser');
 
-server.use('/api', api)
+// const api = require('./api.js')
+
+// const server = express()
+// server.use('/api', api)
+app.use(express.json());
 
 app.get("/", (req, res) => res.type('html').send(html));
+
+app.post("/submitVote", async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      name: "POST endpoint", txHash: "", pollId: -1, success: false
+    })
+  }
+  body = req.body
+  data = body
+  // if ("data" in body == false) {
+  //   return res.status(400).json({
+  //     name: "No data", txHash: "", pollId: -1, success: false
+  //   })
+  // }
+  // var data = body
+
+  var nullifierHash, vote, proof, pollId
+
+  // Required fields!
+  if ("nullifierHash" in data == false) {
+    return res.status(400).json({
+      name: "Must pass in nullifierHash", txHash: "", pollId: -1, success: false
+    })
+  } else {
+    nullifierHash = data.nullifierHash
+  }
+  if ("proof" in data == false) {
+    return res.status(400).json({
+      name: "Must pass in a proof", txHash: "", pollId: -1, success: false
+    })
+  } else {
+    vote = data.vote
+  }
+  if ("vote" in data == false) {
+    return res.status(400).json({
+      name: "Must pass in a vote", txHash: "", pollId: -1, success: false
+    })
+  } else {
+    proof = data.proof
+  }
+  if ("pollId" in data == false) {
+    return res.status(400).json({
+      name: "Must pass in a poll id", txHash: "", pollId: -1, success: false
+    })
+  } else {
+    pollId = data.pollId
+  }
+
+  // TODO: REPLACE WITH PRIV_POLL SMART CONTRACT
+  const PRIV_POLL_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_GOERLI_POLL_CONTRACT
+  const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+  const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+
+  if (typeof(GOERLI_PRIVATE_KEY) == 'undefined') {
+    throw new Error("GOERLI_PRIVATE_KEY not set")
+  }
+  if (typeof(PRIV_POLL_CONTRACT_ADDRESS) == 'undefined') {
+    throw new Error("PRIV_POLL is not set")
+
+  }
+
+  // Initialize wallet from env private variable
+  // Replace with local host endpoint & private key
+  const provider = new ethers.providers.JsonRpcProvider(GOERLI_RPC_URL, "goerli");
+  const wallet = new ethers.Wallet(GOERLI_PRIVATE_KEY, provider);
+  
+  // console.log(wallet.address)
+  const contract = new ethers.Contract(PRIV_POLL_CONTRACT_ADDRESS, privPollABI, wallet);
+
+
+  // TODO: REPLACE DUMMY CALL HERE WITH ACTUAL CALL TO VERIFIER CONTRACT
+  console.log("Right before cast vote")
+  console.log("vote", typeof(vote), vote)
+  console.log("nullifier", typeof(nullifierHash), nullifierHash)
+  console.log("pollId", typeof(pollId), pollId)
+  console.log("proof", typeof(proof), proof)
+  var strVote;
+  if (vote == 0) {
+      strVote =
+      "0x0000000000000000000000000000000000000000000000000000000000000000";
+  } else {
+      strVote =
+      "0x0000000000000000000000000000000000000000000000000000000000000001";
+  }
+  // const tx = await contract.getPollState(pollId)
+  const relayer = "0x426bF8b7C4f5CB67eb838CE2585116598cE3019A"
+  console.log("Relayer", relayer);
+  let receipt
+  try {
+      const tx = await contract.castVote(strVote, nullifierHash, pollId.toString(), proof,
+          {
+          from: relayer,
+          gasLimit: '3000000'
+          })
+      receipt = await tx.wait();
+
+
+  } catch(error) {
+      if (error.code === ethers.utils.Logger.errors.CALL_EXCEPTION) {
+  
+          // The receipt
+          console.log(error.receipt);
+  
+      } else if (error.code === ethers.utils.Logger.errors.TRANSACTION_REPLACED) {
+  
+          // The receipt of the replacement transaction
+          console.log(error.receipt);
+  
+          // The reason ("repriced", "cancelled" or "replaced")
+          console.log(error.reason);
+  
+          // The transaction that replaced this one
+          console.log(error.replacement);
+  
+      } else {
+          // This shouldn't really happen; maybe server error, like the internet connection failed?
+      }
+      res.statusCode = 200
+      return res.json({ name: "Failed to vote!", txHash: error.txHash, pollId: pollId, success: false })
+  }
+  res.statusCode = 200
+  return res.json({ name: "Voted!", txHash: receipt.txHash, pollId: pollId, success: true })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
